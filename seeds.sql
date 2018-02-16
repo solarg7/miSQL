7a641ca96fd50ae41cbd89e1514bf437ce984259
Marcelo Del Solar <solarg7@Marcelos-MacBook-Pro.local>
2018-02-16 15:03:03 -0500

    HW10

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE BamazonDB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+
+("Dark Chocolate", "Grocery", 4.94, 4),
+("Tifany Brazalette", "Jewelery", 212.43, 3),
+("Harry Potter" , "Books", 14.33, 3),
+("Machetazos", "Books", 32.56, 10),
+("Nike 80s", "Shoes", 43.59, 4),
+("PC", "Electronics", 900.00, 4),
+("Apple", "Electronics", 910.00, 3),
+("Kate Spade", "Jewelery", 40.41, 11),
+("ac dc Vinyl", "Music", 41.02, 12),
+("kiss Vinyl", "Music", 34.87, 4),
+("Kids", "Toys", 12.62, 11);
